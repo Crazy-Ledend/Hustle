799ecbd2071b24e7f283b27d3b06217275887a65
Crazy_Pokeking <75431308+Crazy-Ledend@users.noreply.github.com>
2026-02-16 00:23:08 +0530

    Update settings.py

--- hustle/settings.py
+++ hustle/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # USER DEFINED SETTINGS
 LOGIN_URL = "/login/"
